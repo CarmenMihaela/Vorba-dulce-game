50fd6af9b2206edc00e3824dc63767032f7c83b0
Carmen  Vernica <carmen.avadani@gmail.com>
2021-05-14 18:25:26 +0200

    commit

---
+++ letterF.js
@@ -0,0 +1,155 @@
+//lists of pictures and words for each letter.
+var pictureListF = ["https://images.unsplash.com/photo-1594404124434-719a41b02af5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+"https://images.unsplash.com/photo-1524706941600-e33d2fa53919?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+"https://cdn.pixabay.com/photo/2017/08/26/06/27/ficus-2682320_1280.jpg",
+"https://cdn.pixabay.com/photo/2016/09/01/01/43/little-boy-1635065_1280.jpg",
+"https://images.unsplash.com/photo-1612617397200-8362bc943e20?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+"https://images.unsplash.com/photo-1525999147711-835474620964?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1021&q=80",
+"https://cdn.pixabay.com/photo/2013/06/09/17/04/fire-123784_1280.jpg",
+"https://images.unsplash.com/photo-1516298761027-64d49da72287?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80",
+"https://images.unsplash.com/photo-1533227268428-f9ed0900fb3b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=879&q=80",
+"https://images.unsplash.com/photo-1572877821089-85e54c8fd162?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80"]
+var wordListF = ["FASOLE", "FANFARĂ", "FICUS", "FRICĂ", "FERIBOT", "FARFURIE", "FOC", "FAZAN", "FERICIT", "FOSFORESCENT"]
+
+//creating the count to loop through images list
+var nextPic = (function() {
+  var count = -1;
+  return function() {
+    return pictureListF[++count % pictureListF.length];
+  }
+}());
+
+//creating the count to loop through words lists
+var nextWord = (function() {
+  var indice = -1;
+  return function() {
+    return wordListF[++indice % wordListF.length];
+  }
+}());
+
+var correctLetters = []; //the list with correct letters is empty in the beginning
+
+function ready(){//the code that gets executed at page load
+  var totalScore = sessionStorage.getItem("totalWins");//the score made by now is stored in totalScore
+  winCount = Number(totalScore);//winCount is assigned the value of totalScore to continue the count
+  if (winCount >50) {//if the score is bigger than 50, its short version is displayed
+    document.querySelector(".score").innerHTML = "Your score: " + " 🍒 " + winCount;
+  } else {
+    document.querySelector(".score").innerHTML = "Your score: " + "🍒".repeat(winCount) + " " + winCount;
+  }
+
+  document.getElementById("mainbutton").innerHTML = "Next word"; //make the mainbutton show Next word message
+  document.querySelector(".score").classList.remove("glowbox");//remove glow from score
+  document.querySelector(".score").classList.remove("done"); //remove shadows animations score
+  document.getElementById("kidspic").classList.remove("glowbox");//remove glow from image
+  document.querySelector(".word").classList.remove("glowtext");//remove glow from word
+  correctLetters = [];//remove elements from list of guessed letters
+  var letter = document.getElementsByClassName('letter');
+  for (var i=0; i<letter.length;i++){
+    letter[i].classList.remove('bordered');//remove the border from letters when Next word is assigned
+    letter[i].style.textDecoration = "none";//remove text decoration when Next word is assigned
+  }
+  document.getElementById("kidspic").src = nextPic(); //next image is shown
+  var currentWord = nextWord();// the next word is stored in var currentWord
+  var numberOfDashes =  currentWord.length * 1.48; // obtaining the width from currenWord length
+  var wordWidth = numberOfDashes.toString() + "ch";
+  document.querySelector(".word").style.width = wordWidth; //making the dashes for current word
+  document.querySelector(".word").innerHTML = currentWord; //assign the currentWord to word
+  document.querySelector(".word").style.color = "#303030";//make the word invisible;
+  var cleanWord = currentWord.split("");//making an array of the letters in the word
+
+  for (var i=0; i<letter.length;i++) {// looping through letter mainbuttons
+    letter[i].onclick = function(event) {//when a letter mainbutton is clicked
+      this.classList.add("bordered");//when clicked, the letter adds border
+      var theLetter = this.innerHTML;//the value of the letter mainbutton is stored in var
+      var styledLetter = '<span style="color: white;">' + theLetter + '</span>';//use var to make the second part of replace method
+      if (currentWord.includes(theLetter) && !correctLetters.includes(theLetter)) {//if the letter is in the word
+        currentWord = currentWord.replaceAll(theLetter, styledLetter);// the letter becomes visible in all places
+        document.querySelector(".word").innerHTML = currentWord;//keep visibility of the letters already discovered
+        correctLetters.push(theLetter);//all correct letters are added to the array
+        const uniqueCorrectLetters = Array.from(new Set(correctLetters));//array of correct letters is reduced to unique correct letters
+        const uniqueCleanWord = Array.from(new Set(cleanWord));//array made of unique letters in the current word
+        if (uniqueCorrectLetters.length == uniqueCleanWord.length){//if the length of the 2 unique arrays is equal,the winning word has been found
+          winCount+=1;//increasing score with every word won
+          document.getElementById("kidspic").classList.add("glowbox");//makes the picture's border glow
+          document.querySelector(".word").classList.add("glowtext");//add glow to word
+          document.querySelector(".score").innerHTML = "Your score: " + "🍒".repeat(winCount) + " " + winCount;
+          sessionStorage.setItem("totalWins", winCount);//add the new win to the storage
+
+          document.querySelector(".score").classList.add("glowbox");//adding glow to score
+
+          if (winCount >50) {//if the score is bigger than 50, its short version is displayed
+            document.querySelector(".score").innerHTML = "Your score: " + " 🍒 " + winCount;
+          }
+        }
+      } else {// if letter is not in the word
+        this.style.textDecoration = "#DC143C 5px line-through";//cross the letter mainbuttons that are pushed but not part of the word
+      }
+    }
+  }
+}
+
+function readybutton(){//the code that gets executed at button press
+  if (winCount >50) {//if the score is bigger than 50, its short version is displayed
+    document.querySelector(".score").innerHTML = "Your score: " + " 🍒 " + winCount;
+  } else {
+    document.querySelector(".score").innerHTML = "Your score: " + "🍒".repeat(winCount) + " " + winCount;
+  }
+  document.getElementById("mainbutton").innerHTML = "Next word"; //make the mainbutton show Next word message
+  document.querySelector(".score").classList.remove("glowbox");//remove glow from score
+  document.querySelector(".score").classList.remove("done"); //remove shadows animations score
+  document.getElementById("kidspic").classList.remove("glowbox");//remove glow from image
+  document.querySelector(".word").classList.remove("glowtext");//remove glow from word
+  correctLetters = [];//remove elements from list of guessed letters
+  var letter = document.getElementsByClassName('letter');
+  for (var i=0; i<letter.length;i++){
+    letter[i].classList.remove('bordered');//remove the border from letters when Next word is given
+    letter[i].style.textDecoration = "none";//remove text decoration when Next word is given
+  }
+  document.getElementById("kidspic").src = nextPic(); //next image is shown
+  var currentWord = nextWord();// the next word is stored in var currentWord
+  var numberOfDashes =  currentWord.length * 1.48; // obtaining the width from currenWord length
+  var wordWidth = numberOfDashes.toString() + "ch";
+  document.querySelector(".word").style.width = wordWidth; //making the dashes for current word
+  document.querySelector(".word").innerHTML = currentWord;//assign the currentWord to word
+  document.querySelector(".word").style.color = "#303030";//make the word invisible;
+  var cleanWord = currentWord.split("");//new array made of the letters of the correct word
+
+  for (var i=0; i<letter.length;i++) {// looping through letter mainbuttons
+    letter[i].onclick = function(event) {//when a letter mainbutton is clicked
+      this.classList.add("bordered");//when clicked, the letter adds border
+      var theLetter = this.innerHTML;//the value of the letter mainbutton is stored in var
+      var styledLetter = '<span style="color: white;">' + theLetter + '</span>';//use var to make the second part of replace method
+      if (currentWord.includes(theLetter) && !correctLetters.includes(theLetter)) {//if the letter is in the word
+        currentWord = currentWord.replaceAll(theLetter, styledLetter);// the letter becomes visible in all places
+        document.querySelector(".word").innerHTML = currentWord;//keep visibility of the letters already discovered
+        correctLetters.push(theLetter);//all correct letters are added to the array
+        const uniqueCorrectLetters = Array.from(new Set(correctLetters));//array of correct letters is reduced to unique correct letters
+        const uniqueCleanWord = Array.from(new Set(cleanWord));//array made of unique letters in the current word
+
+        if (uniqueCorrectLetters.length == uniqueCleanWord.length){//if the length of the 2 unique arrays is equal,the winning word has been found
+
+          winCount+=1;
+          document.getElementById("kidspic").classList.add("glowbox");//makes the picture's border glow
+          document.querySelector(".word").classList.add("glowtext");//add glow to word
+          document.querySelector(".score").innerHTML = "Your score: " +  "🍒".repeat(winCount) + " " + winCount;
+          sessionStorage.setItem("totalWins", winCount);//the new score is added to the sorage
+
+          //if the picture is the last picture shown
+          if (document.getElementById("kidspic").src === "https://images.unsplash.com/photo-1572877821089-85e54c8fd162?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80") {
+            document.getElementById("mainbutton").innerHTML = "Play letter F again"//the mainbutton shows play again
+          }
+          document.querySelector(".score").classList.add("glowbox");//adding glow to score
+
+          if (winCount >50) {//if the score is bigger than 50, its short version is displayed
+            document.querySelector(".score").innerHTML = "Your score: " + " 🍒 " + winCount;
+          }
+        }
+      } else {// if letter is not in the word
+        this.style.textDecoration = "#DC143C 5px line-through";//cross the letter mainbuttons that are pushed but not part of the word
+      }
+    }
+  }
+}
+document.addEventListener("DOMContentLoaded", ready); //when the page loads, the first word is to be guessed
+document.getElementById("mainbutton").onclick = readybutton; // when the button is pressed, the next word is shown
